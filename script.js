245e405b04d999e94d4ae12cc0d7960756b604f5
Jacob Stoebel <stoebelj@berea.edu>
2015-12-30 12:35:20 -0500

    added cat display

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function clock () {
 	this.work_time = 25.0;		//the user's desired work_time
 	this.break_time = 5.0;	//the user's desired break time
 
-	this.mode = "work";			//set to either work or break
+	this.mode = "Session";			//set to either work or break
 	this.timer = null;			//the value of the active timer (in seconds)
 	this.countdown = false;		//if the clock is counting down
 
@@ -15,9 +15,11 @@ function clock () {
 		if (this.countdown) {
 			//cancel
 			this.cancel()
+			this.timer_setup()
 		} else {
 			//start
 			this.timer_setup()
+			this.start_timer()
 		}
 	}
 
@@ -27,12 +29,9 @@ function clock () {
 		//post: timer is set up and recursive function 
 		//is boostrapped
 
-		$("#start").text("cancel")
-		this.countdown = true
-
 		if (self.timer == null) {
 			//load the timer if its null already.
-			if (this.mode == "work") {
+			if (this.mode == "Session") {
 				this.timer = this.work_time * 60
 			} else if (this.mode == "break") {
 				this.timer = this.break_time * 60
@@ -40,9 +39,14 @@ function clock () {
 		}
 
 		this.update_display()
+	},
 
+	this.start_timer = function() {
+		//pre: timer display has been set up
+		//post: sets the timer to begin countdown
+		$("#start").text("cancel")
+		this.countdown = true
 		this.intervalId = setInterval(this.tick.bind(this), 1000)
-
 	},
 
 	this.tick = function() {
@@ -56,19 +60,44 @@ function clock () {
 		this.update_display();
 
 		//kill interval if timer is at 0
+		//and switch timing modes.
 		if (this.timer == 0) {
-			clearInterval(this.intervalId)
-		}
+			this.countdown_cleanup();
+		};
 
-	};
+	},
+
+	this.countdown_cleanup = function() {
+		//pre: this.timer == 0
+		//post:
+			//kills the active timer
+			//restores timer to pre countdown state and
+			//starts timer on alternate state 
+			//(work -> break) and visa versa.
+
+		//kill the timer
+		clearInterval(this.intervalId)
+
+		//restore state
+		this.countdown = false;
+		this.timer = null;
+		//toggle modes
+		if (this.mode == "Session"){
+			this.mode = "break";
+		} else {
+			this.mode = "Session";
+		};
+		this.start_cancel();
+	},
 
 	this.update_display = function() {
 		//pre: none
 		//post: timer display is updated to reflect
 		//	this.timer
+		// display-outer is updated to reflect this.mode
 
 		$("#display").text(this.timeReadable.bind(this))
-
+		$("#mode").text(this.mode)
 	}
 
 	this.timeReadable = function() {
@@ -97,27 +126,45 @@ function clock () {
 		//pre: none
 		//post: stops and nulls out the timer. 
 
-		//change the button back to start and clear the display
-		$("#start").text("start")
-		this.countdown = false
-		$("#display").text("")
-
-		clearInterval(this.intervalId)
+		//change the button back to start 
+		//and clear the display
+		$("#start").text("start");
+		clearInterval(this.intervalId);
 
 		if (self.countdown == true) {
 			self.countdown = false;
 			self.timer = null
+			this.mode = "Session"
 		}
 	};
+
+	this.pause_resume = function () {
+		//pre: none
+		//handles either pausing or resuming countdown as needed.
+
+		if (this.countdown == true) {
+			this.pause();
+		} else {
+			this.resume();
+		}
+	}
 
 	this.pause = function() {
 		//pre: none
 		//post: pauses the timer leaving the time remaining
+		
+		$("#pause").text("resume");
+		clearInterval(this.intervalId);
 
-		if (self.countdown == true) {
-			self.countdown = false;
-		}
+		if (this.countdown == true) {
+			this.countdown = false;
+		};
 	};
+
+	this.resume = function() {
+		$("#pause").text("pause");
+		this.start_timer();
+	}
 
 	this.inc_work_time = function(spam) {
 
@@ -181,6 +228,7 @@ $(document).ready(function() {
 
 	//start and stop buttons
 	$("#start").click(pom.start_cancel.bind(pom))
+	$("#pause").click(pom.pause_resume.bind(pom))
 
 });
 
